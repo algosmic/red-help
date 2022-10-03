05fd3169b375135ec70b0dcabc6cf50ab7032545
Aryaman Kamat <86251797+aryamankamat@users.noreply.github.com>
2022-10-03 15:50:45 +0530

    Initial commit to Aryaman

---
+++ file_list.py
@@ -0,0 +1,8 @@
+"""
+This is a red team project, We are preparing a Tool(function) that will accept an input from user which will be a path of a file and
+search for certain critial keywords, once it finds out the tool will return to the user in the form of dictionary-->list.
+eg :
+file_name : -------
+keyword_matched : -----
+Directory_in : ------
+"""
