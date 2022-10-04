05d0ce631026e059ea13a3420a76bb6fb038ceab
Aryaman Kamat <86251797+aryamankamat@users.noreply.github.com>
2022-10-04 16:12:06 +0530

    Third commit

--- file_list.py
+++ file_list.py
@@ -1,5 +1,9 @@
-"""
-This is a red team project, We are preparing a Tool(function) that will accept an input from user which will be a path of a file and
+import glob
+import os
+
+
+def file_list():
+    """ This is a red team project, We are preparing a Tool(function) that will accept an input from user which will be a path of a file and
 search for certain critial keywords, once it finds out the tool will return to the user in the form of dictionary-->list.
 eg :
 file_name : -------
@@ -7,25 +11,24 @@ keyword_matched : -----
 Directory_in : ------
 """
 
+    path = r'D:\CYBER-SECURITY\Hacking_BOOKS'
+    # fpath = glob.glob("{0}\**\*".format(path), recursive=True)
+    # fpath = glob.glob("{0}\*.bat".format(path), recursive=True):
+    # fpath = glob.glob("{0}\**\*.py".format(path), recursive=True):
+    # fpath = glob.glob("{0}\**\*.bat".format(path), recursive=True):
+
+    for fpath in glob.glob("{0}\**\*".format(path), recursive=True):
+        fsize = os.stat(fpath).st_size/1024
+        # print('{0} is {1}', format(fpath, fsize))
+        print(fpath, "is", fsize, "KB")
+
+
+# print(file_list.__doc__)
+# print(dir(file_list))
+
+
+# Second Trial
 """
-import glob
-import os
-
-path = r'D:\CYBER-SECURITY\Hacking_BOOKS'
-# fpath = glob.glob("{0}\**\*".format(path), recursive=True)
-# fpath = glob.glob("{0}\*.bat".format(path), recursive=True):
-# fpath = glob.glob("{0}\**\*.py".format(path), recursive=True):
-# fpath = glob.glob("{0}\**\*.bat".format(path), recursive=True):
-
-
-for fpath in glob.glob("{0}\**\*".format(path), recursive=True):
-    fsize = os.stat(fpath).st_size/1024
-    # print('{0} is {1}', format(fpath, fsize))
-    print(fpath, "is", fsize, "KB")
-
-"""
-
-
 #from.import iso
 
 #from six import add_metaclass, iteritems
@@ -36,3 +39,50 @@ iso = isoparser.parse(r'D:\CYBER-SECURITY\Hacking_BOOKS')
 keys = iso.record()
 # print(iso.record("boot", "grub").children)
 # print(iso.record("boot", "grub", "grub.cfg").content)
+"""
+
+
+# Third Trial
+"""
+import glob
+import os
+import uuid  # univeral unique identifier
+
+def change_direct():
+    path = input("Enter a Path:")
+    keyword = input("Enter a Keyword/Phrase to search : ")
+    search_folder(path, keyword)
+    # except:
+    #print("Could not find path. Please try again.")
+    # change_direct()
+
+
+contains = []
+id_ = uuid.uuid4()
+
+
+def search_folder(path, keyword):
+    global contains
+    dirs = os.listdir(path)
+    # for every directory that is listed(which includes files)
+    for directory in dirs:
+        if directory.endswith(".txt"):
+            try:
+                file_ = open(path + "/" + directory, "r")
+                if keyword in file_.read():
+                    contains.append(directory)
+                    saveFile = open(
+                        r"D:\Programming\ALGOSMIC\red-help>" + keyword + " " + str(id_) + ".txt", "a+")
+                    saveFile.write(path + "\\" + directory + "\n")
+                    saveFile.close()
+            except:
+                pass
+        elif "." not in directory.split(" "):
+            search_folder(path + "/" + directory, keyword)
+
+
+change_direct()
+print(contains)
+print("The search results has been saved to the folder defined in the script at line 27.")
+input("Press enter to exit...")
+"""
